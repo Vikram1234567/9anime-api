9af4cdd3930e00a82f6f52ba6260dec0f6bc6d4e
Nunu27 <wisnu.agung.001@gmail.com>
2021-07-18 14:40:24 +0700

    home all done

--- api/client/Gomunime.js
+++ api/client/Gomunime.js
@@ -3,21 +3,85 @@ const cheerio = require("cheerio");
 const base = `https://gomunime.online/`;
 const domain = process.env.DOMAIN ?? "http://localhost:5000/";
 
+// Element Digest
 const parseCard = (e) => {
   const elem = cheerio.default(e);
-  const link = elem.find(".name");
+  const splitUrl = elem.find("a").first().attr("href").split("/");
+  const img = elem.find("img");
+  return {
+    id: splitUrl[4] ? splitUrl[4] : splitUrl[3],
+    title: elem.find(".name").text(),
+    cover: img.data("lazy-src") ?? img.attr("src"),
+    status: elem.find(".status").text(),
+    type: elem.find(".taglist span").first().text(),
+    episode: elem.find(".ep").text(),
+    hot: !!elem.find(".dub").length,
+  };
+};
+const parseCarouselItem = (e) => {
+  const elem = cheerio.default(e);
 
   return {
-    id: link.attr("href").split("/")[4],
-    title: link.text(),
-    cover: elem.find("img").attr("src"),
-    status: elem.find(".status").text(),
-    type: elem.find(".taglist").text().trim(),
-    episode: elem.find(".ep").text(),
+    id: elem.find("a").attr("href").split("/")[4],
+    backdrop: elem.find(".backdrop").data("background"),
+    title: elem.find("h2").text(),
+    synopsis: elem.find("p").text(),
   };
 };
 
+const ajaxRequest = async (query) => {
+  const { data } = await axios({
+    url: base + "wp-admin/admin-ajax.php",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+    },
+    data: new URLSearchParams(query).toString(),
+    method: "POST",
+  });
+  return data;
+};
+const homelist = async (list) => {
+  const data = await ajaxRequest({
+    action: "home_ajax",
+    fungsi: list,
+    pag: 1,
+  });
+
+  const $ = cheerio.load(data.html);
+  const results = $("li").toArray().map(parseCard);
+  return results;
+};
+
 class Gomunime {
+  async home(req, res) {
+    try {
+      const { data } = await axios.get(base);
+      const $ = cheerio.load(data);
+
+      const hl = ["e", "c", "la", "t"];
+      const highlight = $(".swiper-slide").toArray().map(parseCarouselItem);
+      const [terbaru, completed, liveaction, trending] = await Promise.all(
+        hl.map(homelist)
+      );
+      const baru = $(".anime-list").last().find("li").toArray().map(parseCard);
+
+      res.json({
+        success: true,
+        highlight,
+        terbaru,
+        completed,
+        baru,
+        liveaction,
+        trending,
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({
+        success: false,
+        message: error.toString(),
+      });
+    }
+  }
   async search(req, res) {
     try {
       const page = req.query.page ?? 1;
--- api/index.js
+++ api/index.js
@@ -3,7 +3,20 @@ const Gomunime = require("./client/Gomunime");
 const app = require("express")();
 const port = process.env.PORT || 5000;
 
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Gomunime API",
+  });
+});
+app.get("/home", Gomunime.home);
 app.get("/search/:query", Gomunime.search);
+app.use("*", (req, res) => {
+  res.status(404).json({
+    success: false,
+    message: "Path not found",
+  });
+});
 
 app.listen(port, () => console.log("Listening to port", port));
 
